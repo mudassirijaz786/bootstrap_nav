4a83c3fb0cb98a26cb815b45b25adabcfdb357bd
Mudassir Ijaz <ijazmudassir786@gmail.com>
2021-11-22 01:05:03 +0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { selectFilter } from "react-bootstrap-table2-filter";
+import { statusOptions, data } from "./data/constants";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filters: {},
+    };
+
+    this.statusFilter = null;
+  }
+
+  onTableChange = (type, newState) => {
+    this.setState({
+      filters: newState.filters,
+    });
+  };
+
+  resetFilters = () => {
+    this.statusFilter("");
+  };
+
+  render() {
+    const customers = () => {
+      const { filters } = this.state;
+      let items = [...data];
+
+      const statusFilter = filters?.status ? filters?.status.filterVal : null;
+
+      if (statusFilter) {
+        items = items.filter((item) => item.status === statusFilter);
+      }
+
+      return items;
+    };
+
+    const columns = [
+      {
+        dataField: "firstName",
+        text: "First Name",
+      },
+      {
+        dataField: "lastName",
+        text: "Last Name",
+      },
+      {
+        dataField: "carrier",
+        text: "Carrier",
+      },
+      {
+        dataField: "status",
+        text: "Status",
+        filter: selectFilter({
+          options: statusOptions,
+          getFilter: (filter) => {
+            this.statusFilter = filter;
+          },
+        }),
+      },
+      {
+        dataField: "eventDate",
+        text: "Event Date",
+      },
+    ];
+
+    return (
+      <div class="table">
+        <BootstrapTable
+          keyField="id"
+          data={customers()}
+          columns={columns}
+          remote={{ filter: true }}
+          onTableChange={this.onTableChange}
+          filter={filterFactory()}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/data/constants.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    firstName: "Tim",
+    lastName: "Jones",
+    carrier: "Humana",
+    status: "COMPLETE",
+    eventDate: "2021-09-22",
+  },
+  {
+    firstName: "Tim",
+    lastName: "Jones",
+    carrier: "Humana",
+    status: "COMPLETE",
+    eventDate: "2021-09-22",
+  },
+  {
+    firstName: "Bill",
+    lastName: "Jones",
+    carrier: "Humana",
+    status: "ERROR",
+    eventDate: "2021-09-23",
+  },
+  {
+    firstName: "Tim",
+    lastName: "Blue",
+    carrier: "Aetna",
+    status: "INCOMPLETE",
+    eventDate: "2021-09-25",
+  },
+  {
+    firstName: "Jim",
+    lastName: "Red",
+    carrier: "UHC",
+    status: "COMPLETE",
+    eventDate: "2021-09-20",
+  },
+];
+
+export const statusOptions = {
+  COMPLETE: "COMPLETE",
+  ERROR: "ERROR",
+  INCOMPLETE: "INCOMPLETE",
+};
